9d07c249fd6bbac94f8013a3b32d13faa5305294
ChauTriTin <freuss47@gmail.com>
2023-07-15 16:38:48 +0700

    up

--- app/src/main/java/com/roy/downloader/ui/customview/FixHintTextInputEditText.kt
+++ app/src/main/java/com/roy/downloader/ui/customview/FixHintTextInputEditText.kt
@@ -13,47 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.roy.downloader.ui.customview
 
-package com.roy.downloader.ui.customview;
-
-import android.content.Context;
-import android.os.Build;
-import android.util.AttributeSet;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.google.android.material.textfield.TextInputEditText;
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
 
 /*
  * Fixes crash on Android 8.0 if hint is inside TextInputEditText
  * See https://issuetracker.google.com/issues/62834931
  */
+class FixHintTextInputEditText : TextInputEditText {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr)
 
-public class FixHintTextInputEditText extends TextInputEditText {
-
-    public FixHintTextInputEditText(@NonNull Context context) {
-        super(context);
-    }
-
-    public FixHintTextInputEditText(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public FixHintTextInputEditText(
-            @NonNull Context context,
-            @Nullable AttributeSet attrs,
-            int defStyleAttr
-    ) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    @Override
-    public int getAutofillType() {
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
-            return AUTOFILL_TYPE_NONE;
+    override fun getAutofillType(): Int {
+        return if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
+            AUTOFILL_TYPE_NONE
         } else {
-            return super.getAutofillType();
+            super.getAutofillType()
         }
     }
 }
