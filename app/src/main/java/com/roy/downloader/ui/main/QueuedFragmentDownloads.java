e32e8088bc79000f7c44c49b4ffe36b797672e32
ChauTriTin <freuss47@gmail.com>
2023-07-15 17:07:38 +0700

    up

--- app/src/main/java/com/roy/downloader/ui/main/QueuedFragmentDownloads.java
+++ app/src/main/java/com/roy/downloader/ui/main/QueuedFragmentDownloads.java
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2019 Tachibana General Laboratories, LLC
- * Copyright (C) 2019 Yaroslav Pronin <proninyaroslav@mail.ru>
- *
- * This file is part of Download Navi.
- *
- * Download Navi is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Download Navi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Download Navi.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.roy.downloader.ui.main;
 
 import android.os.Bundle;
@@ -26,14 +6,11 @@ import androidx.annotation.NonNull;
 
 import com.roy.downloader.core.model.data.StatusCode;
 
-public class QueuedFragmentDownloads extends FragmentDownloads
-    implements DownloadListAdapter.QueueClickListener
-{
+public class QueuedFragmentDownloads extends FragmentDownloads implements DownloadListAdapter.QueueClickListener {
     @SuppressWarnings("unused")
     private static final String TAG = QueuedFragmentDownloads.class.getSimpleName();
 
-    public static QueuedFragmentDownloads newInstance()
-    {
+    public static QueuedFragmentDownloads newInstance() {
         QueuedFragmentDownloads fragment = new QueuedFragmentDownloads();
         Bundle args = new Bundle();
         fragment.setArguments(args);
@@ -41,34 +18,29 @@ public class QueuedFragmentDownloads extends FragmentDownloads
         return fragment;
     }
 
-    public QueuedFragmentDownloads()
-    {
+    public QueuedFragmentDownloads() {
         super((item) -> !StatusCode.isStatusCompleted(item.info.statusCode));
     }
 
     @Override
-    public void onStart()
-    {
+    public void onStart() {
         super.onStart();
 
         subscribeAdapter();
     }
 
     @Override
-    public void onItemClicked(@NonNull DownloadItem item)
-    {
+    public void onItemClicked(@NonNull DownloadItem item) {
         showDetailsDialog(item.info.id);
     }
 
     @Override
-    public void onItemPauseClicked(@NonNull DownloadItem item)
-    {
+    public void onItemPauseClicked(@NonNull DownloadItem item) {
         viewModel.pauseResumeDownload(item.info);
     }
 
     @Override
-    public void onItemCancelClicked(@NonNull DownloadItem item)
-    {
+    public void onItemCancelClicked(@NonNull DownloadItem item) {
         viewModel.deleteDownload(item.info, true);
     }
 }
