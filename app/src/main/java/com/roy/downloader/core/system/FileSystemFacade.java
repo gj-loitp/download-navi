fe1c0beffca12a89369dd5f109b35c432f8b638c
ChauTriTin <freuss47@gmail.com>
2023-07-15 15:53:29 +0700

    up

--- app/src/main/java/com/roy/downloader/core/system/FileSystemFacade.java
+++ app/src/main/java/com/roy/downloader/core/system/FileSystemFacade.java
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2018-2022 Tachibana General Laboratories, LLC
- * Copyright (C) 2018-2022 Yaroslav Pronin <proninyaroslav@mail.ru>
- *
- * This file is part of Download Navi.
- *
- * Download Navi is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Download Navi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Download Navi.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.roy.downloader.core.system;
 
 import android.net.Uri;
@@ -36,8 +16,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public interface FileSystemFacade
-{
+public interface FileSystemFacade {
     void seek(@NonNull FileOutputStream fout, long offset) throws IOException;
 
     void allocate(@NonNull FileDescriptor fd, long length) throws IOException;
