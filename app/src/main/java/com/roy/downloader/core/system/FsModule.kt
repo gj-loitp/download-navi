d542fff14153475fcccb53de594cc6a6922abd3c
ChauTriTin <freuss47@gmail.com>
2023-07-15 15:56:05 +0700

    up

--- app/src/main/java/com/roy/downloader/core/system/FsModule.kt
+++ app/src/main/java/com/roy/downloader/core/system/FsModule.kt
@@ -1,61 +1,51 @@
-package com.roy.downloader.core.system;
+package com.roy.downloader.core.system
 
-import android.net.Uri;
-
-import androidx.annotation.NonNull;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import android.net.Uri
+import java.io.FileNotFoundException
+import java.io.IOException
 
 /*
  * A platform dependent filesystem interface, that uses in FileSystemFacade.
  */
-
-interface FsModule {
-    String getName(@NonNull Uri filePath);
+internal interface FsModule {
+    fun getName(filePath: Uri): String?
 
     /*
      * Returns path (if present) or directory name
      */
-
-    String getDirName(@NonNull Uri dir);
+    fun getDirName(dir: Uri): String?
 
     /*
      * Returns Uri of the file by the given file name or
      * null if the file doesn't exists
      */
-
-    Uri getFileUri(@NonNull Uri dir, @NonNull String fileName, boolean create) throws IOException;
+    @Throws(IOException::class)
+    fun getFileUri(dir: Uri, fileName: String, create: Boolean): Uri?
 
     /*
      * Returns a file (if exists) Uri by relative path (e.g foo/bar.txt)
      * from the pointed directory
      */
+    @Throws(IOException::class)
+    fun getFileUri(relativePath: String, dir: Uri, create: Boolean): Uri?
 
-    Uri getFileUri(@NonNull String relativePath, @NonNull Uri dir, boolean create) throws IOException;
-
-    boolean delete(@NonNull Uri filePath) throws FileNotFoundException;
-
-    boolean exists(@NonNull Uri filePath);
-
-    FileDescriptorWrapper openFD(@NonNull Uri path);
+    @Throws(FileNotFoundException::class)
+    fun delete(filePath: Uri): Boolean
+    fun exists(filePath: Uri): Boolean
+    fun openFD(path: Uri): FileDescriptorWrapper?
 
     /*
      * Return the number of bytes that are free on the file system
      * backing the given Uri
      */
-
-    long getDirAvailableBytes(@NonNull Uri dir) throws IOException;
-
-    long getFileSize(@NonNull Uri filePath);
-
-    void takePermissions(@NonNull Uri path);
+    @Throws(IOException::class)
+    fun getDirAvailableBytes(dir: Uri): Long
+    fun getFileSize(filePath: Uri): Long
+    fun takePermissions(path: Uri)
 
     /*
      * Returns path (if present) or directory name
      */
-
-    String getDirPath(@NonNull Uri dir);
-
-    boolean mkdirs(@NonNull Uri dir, @NonNull String relativePath);
+    fun getDirPath(dir: Uri): String?
+    fun mkdirs(dir: Uri, relativePath: String): Boolean
 }
