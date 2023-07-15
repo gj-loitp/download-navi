21069370e906b0aafc8689aea1f2a9ab47ed3a78
ChauTriTin <freuss47@gmail.com>
2023-07-15 16:39:29 +0700

    up

--- app/src/main/java/com/roy/downloader/ui/customview/NestedWebView.java
+++ app/src/main/java/com/roy/downloader/ui/customview/NestedWebView.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2015 takahirom
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.roy.downloader.ui.customview;
 
 import android.content.Context;
@@ -62,7 +46,6 @@ public class NestedWebView extends WebView implements NestedScrollingChild2 {
     }
 
 
-
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
         // Initialize a tracker to return if this motion event is handled.
@@ -70,7 +53,7 @@ public class NestedWebView extends WebView implements NestedScrollingChild2 {
 
         // Run the commands for the given motion event action.
         switch (motionEvent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_DOWN -> {
                 // Start nested scrolling along the vertical axis.  `ViewCompat` must be used until the minimum API >= 21.
                 startNestedScroll(ViewCompat.SCROLL_AXIS_VERTICAL);
 
@@ -79,9 +62,8 @@ public class NestedWebView extends WebView implements NestedScrollingChild2 {
 
                 // Run the default commands.
                 motionEventHandled = super.onTouchEvent(motionEvent);
-                break;
-
-            case MotionEvent.ACTION_MOVE:
+            }
+            case MotionEvent.ACTION_MOVE -> {
                 // Get the current Y position.
                 int currentYMotionPosition = (int) motionEvent.getY();
 
@@ -124,15 +106,14 @@ public class NestedWebView extends WebView implements NestedScrollingChild2 {
 
                 // Run the default commands.
                 motionEventHandled = super.onTouchEvent(motionEvent);
-                break;
-
-
-            default:
+            }
+            default -> {
                 // Stop nested scrolling.
                 stopNestedScroll();
 
                 // Run the default commands.
                 motionEventHandled = super.onTouchEvent(motionEvent);
+            }
         }
 
         // Perform a click.  This is required by the Android accessibility guidelines.
