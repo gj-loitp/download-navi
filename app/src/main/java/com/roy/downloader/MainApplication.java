07895278d7b999f539165205e3f1e7fa4412ec11
ChauTriTin <freuss47@gmail.com>
2023-07-15 15:11:19 +0700

    up

--- app/src/main/java/com/roy/downloader/MainApplication.java
+++ app/src/main/java/com/roy/downloader/MainApplication.java
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2019-2021 Tachibana General Laboratories, LLC
- * Copyright (C) 2019-2021 Yaroslav Pronin <proninyaroslav@mail.ru>
- *
- * This file is part of Download Navi.
- *
- * Download Navi is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Download Navi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Download Navi.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.roy.downloader;
 
 import android.util.Log;
@@ -51,18 +31,21 @@ public class MainApplication extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
 
+        setupApp();
+    }
+
+    private void setupApp() {
         CoreConfigurationBuilder builder = new CoreConfigurationBuilder();
         builder
                 .withBuildConfigClass(BuildConfig.class)
                 .withReportFormat(StringFormat.JSON);
         builder.withPluginConfigurations(new MailSenderConfigurationBuilder()
-                .withMailTo("proninyaroslav@mail.ru")
+                .withMailTo("roy93group@gmail.com")
                 .build());
         builder.withPluginConfigurations(new DialogConfigurationBuilder()
                 .withEnabled(true)
                 .withReportDialogClass(ErrorReportActivity.class)
                 .build());
-        // Set stub handler
         if (Thread.getDefaultUncaughtExceptionHandler() == null) {
             Thread.setDefaultUncaughtExceptionHandler((t, e) ->
                     Log.e(TAG, "Uncaught exception in " + t + ": " + Log.getStackTraceString(e))
