4f26aee4912ff28a3e6f0933da5172cf86707a7c
ChauTriTin <freuss47@gmail.com>
2023-07-15 16:04:12 +0700

    up

--- app/src/main/java/com/roy/downloader/core/system/SystemFacadeHelper.kt
+++ app/src/main/java/com/roy/downloader/core/system/SystemFacadeHelper.kt
@@ -1,48 +1,25 @@
-/*
- * Copyright (C) 2019-2022 Tachibana General Laboratories, LLC
- * Copyright (C) 2019-2022 Yaroslav Pronin <proninyaroslav@mail.ru>ru>
- *
- * This file is part of Download Navi.
- *
- * Download Navi is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Download Navi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Download Navi.  If not, see <http://www.gnu.org/licenses/>.
- */
+package com.roy.downloader.core.system
 
-package com.roy.downloader.core.system;
+import android.content.Context
 
-import android.content.Context;
+object SystemFacadeHelper {
+    private var systemFacade: SystemFacade? = null
+    private var fileSystemFacade: FileSystemFacade? = null
 
-import androidx.annotation.NonNull;
-
-public class SystemFacadeHelper
-{
-    private static SystemFacade systemFacade;
-    private static FileSystemFacade fileSystemFacade;
-
-    public synchronized static SystemFacade getSystemFacade(@NonNull Context appContext)
-    {
-        if (systemFacade == null)
-            systemFacade = new SystemFacadeImpl(appContext);
-
-        return systemFacade;
+    @JvmStatic
+    @Synchronized
+    fun getSystemFacade(appContext: Context): SystemFacade? {
+        if (systemFacade == null) systemFacade = SystemFacadeImpl(appContext)
+        return systemFacade
     }
 
-    public synchronized static FileSystemFacade getFileSystemFacade(@NonNull Context appContext)
-    {
-        if (fileSystemFacade == null)
-            fileSystemFacade = new FileSystemFacadeImpl(new SysCallImpl(),
-                    new FsModuleResolverImpl(appContext), appContext);
-
-        return fileSystemFacade;
+    @JvmStatic
+    @Synchronized
+    fun getFileSystemFacade(appContext: Context): FileSystemFacade? {
+        if (fileSystemFacade == null) fileSystemFacade = FileSystemFacadeImpl(
+            SysCallImpl(),
+            FsModuleResolverImpl(appContext), appContext
+        )
+        return fileSystemFacade
     }
 }
