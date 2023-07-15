0fa713891d2b9645ca0aa8650633b00f08d7bef5
ChauTriTin <freuss47@gmail.com>
2023-07-15 15:49:22 +0700

    up

--- app/src/main/java/com/roy/downloader/core/storage/BrowserRepositoryImpl.kt
+++ app/src/main/java/com/roy/downloader/core/storage/BrowserRepositoryImpl.kt
@@ -1,43 +1,27 @@
-package com.roy.downloader.core.storage;
+package com.roy.downloader.core.storage
 
-import androidx.annotation.NonNull;
+import com.roy.downloader.core.model.data.entity.BrowserBookmark
+import io.reactivex.Flowable
+import io.reactivex.Single
 
-import com.roy.downloader.core.model.data.entity.BrowserBookmark;
-
-import java.util.List;
-
-import io.reactivex.Flowable;
-import io.reactivex.Single;
-
-public class BrowserRepositoryImpl implements BrowserRepository {
-    private final AppDatabase db;
-
-    public BrowserRepositoryImpl(@NonNull AppDatabase db) {
-        this.db = db;
+class BrowserRepositoryImpl(private val db: AppDatabase) : BrowserRepository {
+    override fun addBookmark(bookmark: BrowserBookmark): Single<Long> {
+        return db.browserBookmarksDao().add(bookmark)
     }
 
-    @Override
-    public Single<Long> addBookmark(BrowserBookmark bookmark) {
-        return db.browserBookmarksDao().add(bookmark);
+    override fun deleteBookmarks(bookmarks: List<BrowserBookmark>): Single<Int> {
+        return db.browserBookmarksDao().delete(bookmarks)
     }
 
-    @Override
-    public Single<Integer> deleteBookmarks(List<BrowserBookmark> bookmarks) {
-        return db.browserBookmarksDao().delete(bookmarks);
+    override fun updateBookmark(bookmark: BrowserBookmark): Single<Int> {
+        return db.browserBookmarksDao().update(bookmark)
     }
 
-    @Override
-    public Single<Integer> updateBookmark(BrowserBookmark bookmark) {
-        return db.browserBookmarksDao().update(bookmark);
+    override fun getBookmarkByUrlSingle(url: String): Single<BrowserBookmark> {
+        return db.browserBookmarksDao().getByUrlSingle(url)
     }
 
-    @Override
-    public Single<BrowserBookmark> getBookmarkByUrlSingle(String url) {
-        return db.browserBookmarksDao().getByUrlSingle(url);
-    }
-
-    @Override
-    public Flowable<List<BrowserBookmark>> observeAllBookmarks() {
-        return db.browserBookmarksDao().observeAll();
+    override fun observeAllBookmarks(): Flowable<List<BrowserBookmark>> {
+        return db.browserBookmarksDao().observeAll()
     }
 }
